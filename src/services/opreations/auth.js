e8aa52c8f13f2a5038ea1fedc3c2de9a06ab4a95
thezeeshann <info.zeeshann@gmail.com>
2023-10-01 21:55:07 +0530

    Backend api operations

--- src/services/opreations/auth.js
+++ src/services/opreations/auth.js
@@ -1,70 +1,128 @@
-import {setToken,setLoading} from "../../redux/slices/authSlice"
-import {toast} from "react-toastify"
-import {apiConnector} from "../apiConnetor"
-import { authEndpoints } from "../apis"
+import { setToken, setLoading } from "../../redux/slices/authSlice";
+import { toast } from "react-toastify";
+import { apiConnector } from "../apiConnetor";
+import { authEndpoints } from "../apis";
+import { setUser } from "../../redux/slices/userSlice";
 
-const {SIGNUP_API,LOGIN_API} = authEndpoints
+const { SIGNUP_API, LOGIN_API, GET_USER_DETAIL_API, GET_MY_BLOGS_API } =
+  authEndpoints;
 
-
-export function signUp( firstName, lastName, email, password, confirmPassword,navigate){
-    return async(dispatch)=>{
-        const toastId = toast.loading("Loading...")
-        dispatch(setLoading)
-        try {
-            const response = await apiConnector("POST",SIGNUP_API,{
-                firstName,
-                lastName,
-                email,
-                password,
-                confirmPassword
-            })
-            console.log("SIGNUP API RESPONSE............", response)
-            if(!response.data.success){
-                throw new Error(response.data.message)
-            }
-            toast.success("Signup Successful")
-            navigate("/login")
-        } catch (error) {
-            console.log("SIGNUP API ERROR............", error)
-            toast.error("Signup Failed")
-            navigate("/signup")
-        }
-        dispatch(setLoading(false))
-        toast.dismiss(toastId)
+export function signUp(
+  firstName,
+  lastName,
+  email,
+  password,
+  confirmPassword,
+  navigate
+) {
+  return async (dispatch) => {
+    const toastId = toast.loading("Loading...");
+    dispatch(setLoading(true));
+    try {
+      const response = await apiConnector("POST", SIGNUP_API, {
+        firstName,
+        lastName,
+        email,
+        password,
+        confirmPassword,
+      });
+      console.log("SIGNUP API RESPONSE............", response);
+      if (!response.data.success) {
+        throw new Error(response.data.message);
+      }
+      toast.success("Signup Successful");
+      navigate("/login");
+    } catch (error) {
+      console.log("SIGNUP API ERROR............", error);
+      toast.error(error.response.data.message);
+      navigate("/signup");
     }
+    dispatch(setLoading(false));
+    toast.dismiss(toastId);
+  };
 }
 
-export function login(email,password,navigate){
-    return async(dispatch)=>{
-        const toastId = toast.loading("Loading...")
-        dispatch(setLoading)
-        try {
-            const response = await apiConnector("POST",LOGIN_API,{
-                email,
-                password
-            })
-            console.log("LOGIN API RESPONSE............", response)
-            if(!response.data.success){
-                throw new Error(response.data.message)
-            }
-            toast.success("Login Successful")
-            dispatch(setToken(response.data.token))
-            localStorage.setItem("token",JSON.stringify(response.data.token))
-            navigate("/profile");
-        } catch (error) {
-            console.log("LOGIN API ERROR............", error)
-            toast.error("Login Failed")
-        }
-        dispatch(setLoading(false))
-        toast.dismiss(toastId)
+export function login(email, password, navigate) {
+  return async (dispatch) => {
+    const toastId = toast.loading("Loading...");
+    dispatch(setLoading(true));
+    try {
+      const response = await apiConnector("POST", LOGIN_API, {
+        email,
+        password,
+      });
+      console.log("LOGIN API RESPONSE............", response);
+      if (!response.data.success) {
+        throw new Error(response.data.message);
+      }
+      toast.success("Login Successful");
+      dispatch(setToken(response.data.token));
+      dispatch(setUser(response.data.existsUser))
+      localStorage.setItem("token", JSON.stringify(response.data.token));
+      console.log(response.data.existsUser)
+      navigate("/profile");
+    } catch (error) {
+      console.log("LOGIN API ERROR............", error);
+      toast.error("Login Failed");
     }
+    dispatch(setLoading(false));
+    toast.dismiss(toastId);
+  };
 }
 
-export function logout(navigate){
-    return (dispatch)=>{
-        dispatch(setToken(null))
-        localStorage.removeItem("token")
-        toast.success("Logged Out")
-        navigate("/")
+export function logout(navigate) {
+  return (dispatch) => {
+    dispatch(setToken(null));
+    dispatch(setUser(null))
+    localStorage.removeItem("token");
+    localStorage.removeItem("user");
+    toast.success("Logged Out");
+    navigate("/");
+  };
+}
+
+export function getAllUserDetails(token, navigate) {
+  return async (dispatch) => {
+    const toastId = toast.loading("Loading...");
+    dispatch(setLoading(true));
+    try {
+      const response = await apiConnector("GET", GET_USER_DETAIL_API, null, {
+        Authorization: `Bearer ${token}`,
+      });
+      console.log("GET_USER_DETAILS API RESPONSE............", response);
+
+      if (!response.data.success) {
+        throw new Error(response.data.message);
+      }
+      dispatch(setUser(response.data.data));
+    } catch (error) {
+      dispatch(logout(navigate));
+      console.log("GET_USER_DETAILS API ERROR............", error);
+      toast.error("Could Not Get User Details");
     }
+    toast.dismiss(toastId);
+    dispatch(setLoading(false));
+  };
+}
+
+export async function getMyBlogs(token) {
+  let result = [];
+  const toastId = toast.loading("Loading....");
+  try {
+    const response = await apiConnector("GET", GET_MY_BLOGS_API, null, {
+      Authorization: `Bearer ${token}`,
+    });
+    console.log("GET_USER_BLOG API RESPONSE............", response);
+
+    if (!response.data.success) {
+      throw new Error(response.data.message);
+    }
+    result = response?.data?.data;
+    console.log(response.data.data);
+  } catch (error) {
+    console.log("GET_MY_BLOGS API ERROR............", error);
+    toast.error("Could Not Get User Blog Details");
+  }
+  toast.dismiss(toastId);
+  return result;
 }
--- src/services/opreations/blog.js
+++ src/services/opreations/blog.js
@@ -5,15 +5,39 @@ import { blogEndpoints } from "../apis";
 
 const {
   GET_ALL_BLOGS_API,
-  // GET_SINGLE_BLOG_API,
-  // CREATE_BLOG_API,
-  // UPDATE_BLOG_API,
+  CREATE_BLOG_API,
+  UPDATE_BLOG_API,
   DELETE_BLOG_API,
+  CREATE_COMMENT_API,
+  DELETE_COMMENT_API,
 } = blogEndpoints;
+
+export async function createBlog(data, navigate, token) {
+  // return async (dispatch)=>{
+  const toastId = toast.loading("Loading....");
+  // dispatch(setLoading(true));
+  try {
+    const response = await apiConnector("POST", CREATE_BLOG_API, data, {
+      "Content-Type": "multipart/form-data",
+      Authorization: `Bearer ${token}`,
+    });
+    console.log("CREATE BLOG API RESPONSE............", response);
+    if (!response.data.success) {
+      throw new Error(response.data.message);
+    }
+    toast.success("Blog Created Successful");
+    navigate("/");
+  } catch (error) {
+    console.log("CREATE BLOG API ERROR............", error);
+    toast.error(error);
+  }
+  toast.dismiss(toastId);
+  // dispatch(setLoading(false));
+  // }
+}
 
 export function getAllBlogs() {
   return async (dispatch) => {
-    // const toastId = toast.loading("Loadng...");
     dispatch(setLoading(true));
     try {
       const response = await apiConnector("GET", GET_ALL_BLOGS_API);
@@ -26,25 +50,97 @@ export function getAllBlogs() {
       console.log("ALL BLOGS API ERROR............", error);
     }
     dispatch(setLoading(false));
-    // toast.dismiss(toastId);
   };
 }
 
-export async function deleteBlog(blogId, navigate) {
+export async function editBlog(data, token, navigate) {
+  let result = null;
   const toastId = toast.loading("Loading...");
   try {
-    const response = await apiConnector("DELETE", DELETE_BLOG_API, blogId);
-    console.log("DELETE COURSE API RESPONSE............", response);
+    const response = await apiConnector("PUT", UPDATE_BLOG_API, data, {
+      "Content-Type": "multipart/form-data",
+      Authorization: `Bearer ${token}`,
+    });
+    console.log("UPDATE BLOG API RESPONSE............", response);
+    if (!response?.data?.success) {
+      throw new Error("Could Not Update Course Details")
+    }
+    toast.success("Blog Updated Successful");
+    result = response?.data?.data;
+    navigate("/profile");
+  } catch (error) {
+    console.log("UPDATE BLOG API ERROR............", error);
+    toast.error(error.message);
+  }
+  toast.dismiss(toastId);
+  return result;
+}
+
+export async function deleteBlog(blogId, token, navigate) {
+  const toastId = toast.loading("Loading...");
+  try {
+    const response = await apiConnector("DELETE", DELETE_BLOG_API, blogId, {
+      Authorization: `Bearer ${token}`,
+    });
+    console.log("DELETE BLOG API RESPONSE............", response);
     if (!response?.data?.success) {
       throw new Error("Could Not Delete Course");
     }
     toast.success("Blog Deleted");
     navigate("/");
   } catch (error) {
-    console.log("DELETE COURSE API ERROR............", error);
+    console.log("DELETE BLOG API ERROR............", error);
     toast.error(error.message);
   }
   toast.dismiss(toastId);
 }
 
-// loggin to comment on this blog
+// comment
+export async function createComment(data, token) {
+  // return async (dispatch) => {
+  //   dispatch(setLoading(true));
+  const toastId = toast.loading("Loading...");
+  let result = null;
+  try {
+    const response = await apiConnector("POST", CREATE_COMMENT_API, data, {
+      Authorization: `Bearer ${token}`,
+    });
+    console.log("CREATE COMMENT REPOSE", response);
+    if (!response.data.success) {
+      throw new Error(response.data.message);
+    }
+    toast.success("Comment posted");
+    result = response?.data?.data;
+  } catch (error) {
+    console.log("CREATE COMMENT API ERROR............", error);
+    toast.error(error.message);
+  }
+
+  toast.dismiss(toastId);
+  return result;
+  //   dispatch(setLoading(false));
+  // };
+}
+
+export async function deleteComment(commentId, token) {
+  const toastId = toast.loading("Loading....");
+  try {
+    const response = await apiConnector(
+      "DELETE",
+      DELETE_COMMENT_API,
+      commentId,
+      {
+        Authorization: `Bearer ${token}`,
+      }
+    );
+    console.log("DELETE COMMENT API RESPONSE............", response);
+    if (!response?.data?.success) {
+      throw new Error("Could Not Delete Course");
+    }
+    toast.success("Comment Deleted");
+  } catch (error) {
+    console.log("DELETE COMMENT API ERROR............", error);
+    toast.error(error.message);
+  }
+  toast.dismiss(toastId);
+}
