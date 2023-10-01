9cc3139f0274bf61f1e56b7534a95f8429cfe353
thezeeshann <info.zeeshann@gmail.com>
2023-10-01 21:53:24 +0530

    add update and delete api endpoints

--- src/services/apis.js
+++ src/services/apis.js
@@ -3,6 +3,8 @@ const BASE_URL = process.env.REACT_APP_BASE_URL;
 export const authEndpoints = {
   SIGNUP_API: BASE_URL + "/auth/signup",
   LOGIN_API: BASE_URL + "/auth/login",
+  GET_USER_DETAIL_API:BASE_URL + "/auth/getAllUserDetails",
+  GET_MY_BLOGS_API:BASE_URL+"/auth/myAllBlogs"
 };
 
 export const blogEndpoints = {
@@ -11,5 +13,6 @@ export const blogEndpoints = {
     GET_SINGLE_BLOG_API:BASE_URL+"/blog/getSingleBlog",
     UPDATE_BLOG_API:BASE_URL+"/blog/updateBlog",
     DELETE_BLOG_API:BASE_URL+"/blog/deleteBlog",
-    CREATE_COMMENT_API:BASE_URL+"/blog/createComment"
+    CREATE_COMMENT_API:BASE_URL+"/blog/createComment",
+    DELETE_COMMENT_API:BASE_URL+"/blog/deleteComment"
 }
